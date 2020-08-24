1d4f811b4d9169a84852ef7aa30d3591f0aa83a7
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-24 19:49:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,6 @@
 #include <iostream>
 #include <assert.h>    
 
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor)
-{
-    TelCoColorCoder::ColorPair colorPair =
-        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor);
-}
-
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber)
-{
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
-    std::cout << "Got pair number " << pairNumber << std::endl;
-    assert(pairNumber == expectedPairNumber);
-}
-
-void  displayManual()
-{
-    int numberOfMinorColors;
-    int numberOfMajorColors;
-    int pairNumber;
-    int totalPairs=numberOfMinorColors*numberOfMajorColors;
-    std::cout << std::endl;;
-    std::cout << "Color Code Manual"<< std::endl;;
-    for(pairNumber=1;pairNumber<=totalPairs;pairNumber++)
-    {
-        TelCoColorCoder::ColorPair colorPair =
-            TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-        std::cout << pairNumber<<"." << colorPair.ToString() << std::endl;
-    }
-
-}
 
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
