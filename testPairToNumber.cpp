ec3ca123040a3fbf56db9600b910366233fd3511
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-24 20:08:13 +0530

    Update testPairToNumber.cpp

--- testPairToNumber.cpp
+++ testPairToNumber.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <assert.h>    
 
-using namespace TelCoColorCoder;
+
 
 void testPairToNumber(
     TelCoColorCoder::MajorColor major,
     TelCoColorCoder::MinorColor minor,
     int expectedPairNumber)
 {
+    using namespace TelCoColorCoder;
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
