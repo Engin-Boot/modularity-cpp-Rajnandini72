40b4c02e10c6fbe4ca343969d636d49148ff878d
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-24 19:48:47 +0530

    Create displayManual.cpp

---
+++ displayManual.cpp
@@ -0,0 +1,16 @@
+void  displayManual()
+{
+    int numberOfMinorColors;
+    int numberOfMajorColors;
+    int pairNumber;
+    int totalPairs=numberOfMinorColors*numberOfMajorColors;
+    std::cout << std::endl;;
+    std::cout << "Color Code Manual"<< std::endl;;
+    for(pairNumber=1;pairNumber<=totalPairs;pairNumber++)
+    {
+        TelCoColorCoder::ColorPair colorPair =
+            TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+        std::cout << pairNumber<<"." << colorPair.ToString() << std::endl;
+    }
+
+}
