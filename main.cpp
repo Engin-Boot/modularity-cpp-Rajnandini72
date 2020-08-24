3146ec597047241e6fd00f766eebef598126aa6b
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-24 19:37:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -73,12 +73,30 @@ void testPairToNumber(
     assert(pairNumber == expectedPairNumber);
 }
 
+void  displayManual()
+{
+    int numberOfMinorColors;
+    int numberOfMajorColors;
+    int pairNumber;
+    int totalPairs=numberOfMinorColors*numberOfMajorColors;
+    std::cout << std::endl;;
+    std::cout << "Color Code Manual"<< std::endl;;
+    for(pairNumber=1;pairNumber<=totalPairs;pairNumber++)
+    {
+        TelCoColorCoder::ColorPair colorPair =
+            TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+        std::cout << pairNumber<<"." << colorPair.ToString() << std::endl;
+    }
+
+}
+
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    displayManual();
 
     return 0;
 }
