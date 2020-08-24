e22b7451da40d8d68e6bb7ff88257b8135ee413b
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-24 19:46:27 +0530

    Create testNumberToPair.cpp

---
+++ testNumberToPair.cpp
@@ -0,0 +1,12 @@
+
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
