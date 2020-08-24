0e157b627eab26ed0b7249c435443a142a0d84de
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-24 22:32:42 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <assert.h>    
-using namespace TelCoColorCoder;
+#include<TelCoColorCoder.h>
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
+{
+    using namespace TelCoColorCoder;
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+
 
 int main() {
     //using namespace TelCoColorCoder;
