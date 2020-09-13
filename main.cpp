bd930a40835eff21fc28b4ed4e8fc658414703c8
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-09-13 19:39:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 #include <assert.h>    
 #include "namespace.h"
 
@@ -24,31 +25,35 @@ void testPairToNumber(
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
 }
-void  ToString()                          
+std::string ToString()
 {
     using namespace TelCoColorCoder;
     int pairNumber;
-    int totalPairs=numberOfMinorColors*numberOfMajorColors;
+    int totalPairs = numberOfMinorColors * numberOfMajorColors;
+    std::string Manual;
+    std::string subString;
     std::cout << std::endl;;
-    std::cout << "Color Code Manual"<< std::endl;;
-    for(pairNumber=1;pairNumber<=totalPairs;pairNumber++)
+    std::cout << "Color Code Manual" << std::endl;;
+    for (pairNumber = 1; pairNumber <= totalPairs; pairNumber++)
     {
         TelCoColorCoder::ColorPair colorPair =
             TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-        std::cout << pairNumber<<"." << colorPair.ToString() << std::endl;
+        subString = std::to_string(pairNumber)+ "."+colorPair.ToString()+"\n";
+        Manual += subString;
     }
-
+    return Manual;
 }
 
 int main() {
-    
+
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-    
-    ToString();
+
+    std::string manual=ToString();
+    std::cout << manual << std::endl;
 
     return 0;
 }
